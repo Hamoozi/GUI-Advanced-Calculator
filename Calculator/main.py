02ee033ed0eb0ceb65a0a4e23edbc32d740778fe
TheColorofH <82399910+TheColorofH@users.noreply.github.com>
2021-12-31 07:29:15 -0500

    Added Multiplication, Addition, Subtraction and Division.

--- Calculator/main.py
+++ Calculator/main.py
@@ -4,10 +4,12 @@ from tkinter import *
 stack = ArrayStack()
 
 root = Tk()
-root.title("Expressions calculator")
+root.title("Calculator")
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+x = 0
+list1 = []
 
 def button_click(number):
     current = e.get()
@@ -18,38 +20,88 @@ def button_click(number):
 def button_clear():
     e.delete(0, END)
     
-def button_add():
-    stack.push(int(e.get()))
+def button_add(m):
+    global math
+    math = "addition"
+    y = e.get()
     e.delete(0, END)
+    if stack.is_empty() == True:
+        stack.push(int(y))
+    else:
+        stack.push(stack.pop() + int(y))
 
     
 def button_equal():
-    y = e.get()
+    
+    u = e.get()
     e.delete(0, END)
     x = 0
+    
     while stack.is_empty() != True:
         x += stack.pop()
+            
+    if math == "addition":
+        e.insert(0, x + int(u))
+    if math == "multi":
+        e.insert(0, x * int(u))
+    if math == "subtract":
+        e.insert(0, x - int(u))
+    if math == "divide":
+        e.insert(0, x // int(u))
     
     
-    e.insert(0, x+int(y))
+def button_mult():
+    global math
+    math = "multi"
+    
+    y = e.get()
+    e.delete(0, END)
+    if stack.is_empty() == True:
+        stack.push(int(y))
+    else:
+        stack.push(stack.pop() * int(y))
+
+def button_subtract():
+    global math
+    math = "subtract"
+    
+    y = e.get()
+    e.delete(0, END)
+    if stack.is_empty() == True:
+        stack.push(int(y))
+    else:
+        stack.push(stack.pop() - int(y))
+def button_divide():
+    global math
+    math = "divide"
+    
+    y = e.get()
+    e.delete(0, END)
+    if stack.is_empty() == True:
+        stack.push(int(y))
+    else:
+        stack.push(stack.pop() // int(y))
 
         
     
 
-button1 = Button(root, text=1, padx= 40, pady=20, command=lambda: button_click(1))
-button2 = Button(root, text=2, padx= 40, pady=20, command=lambda: button_click(2))
-button3 = Button(root, text=3, padx= 40, pady=20, command=lambda: button_click(3))
-button4 = Button(root, text=4, padx= 40, pady=20, command=lambda: button_click(4))
-button5 = Button(root, text=5, padx= 40, pady=20, command=lambda: button_click(5))
-button6 = Button(root, text=6, padx= 40, pady=20, command=lambda: button_click(6))
-button7 = Button(root, text=7, padx= 40, pady=20, command=lambda: button_click(7))
-button8 = Button(root, text=8, padx= 40, pady=20, command=lambda: button_click(8))
-button9 = Button(root, text=9, padx= 40, pady=20, command=lambda: button_click(9))
-button0 = Button(root, text=0, padx= 40, pady=20, command=lambda: button_click(0))
+button1 = Button(root, text=1, padx= 40, pady=40, command=lambda: button_click(1))
+button2 = Button(root, text=2, padx= 40, pady=40, command=lambda: button_click(2))
+button3 = Button(root, text=3, padx= 40, pady=40, command=lambda: button_click(3))
+button4 = Button(root, text=4, padx= 40, pady=40, command=lambda: button_click(4))
+button5 = Button(root, text=5, padx= 40, pady=40, command=lambda: button_click(5))
+button6 = Button(root, text=6, padx= 40, pady=40, command=lambda: button_click(6))
+button7 = Button(root, text=7, padx= 40, pady=40, command=lambda: button_click(7))
+button8 = Button(root, text=8, padx= 40, pady=40, command=lambda: button_click(8))
+button9 = Button(root, text=9, padx= 40, pady=40, command=lambda: button_click(9))
+button0 = Button(root, text=0, padx= 40, pady=40, command=lambda: button_click(0))
 
-buttonadd = Button(root, text='+', padx=40, pady=20, command=lambda: button_add())
-buttonequal = Button(root, text='=', padx=40, pady=20, command=lambda: button_equal())
-buttonclear = Button(root, text='clear', padx=40, pady=20, command=lambda: button_clear())
+buttonadd = Button(root, text='+', padx=40, pady=40, command=lambda m = "Yo" : button_add(m))
+buttonequal = Button(root, text='=', padx=40, pady=40, command=lambda: button_equal())
+buttonclear = Button(root, text='clear', padx=40, pady=40, command=lambda: button_clear())
+buttonmult = Button(root, text='x', padx=40, pady=40, command=lambda : button_mult())
+buttonsub = Button(root, text='-', padx=40, pady=40, command=lambda: button_subtract())
+buttondiv = Button(root, text='÷', padx=40, pady=40, command=lambda: button_divide())
 
 
 
@@ -67,9 +119,12 @@ button9.grid(row=1, column=2)
 
 button0.grid(row=4, column=0)
 
-buttonadd.grid(row=4, column=2)
-buttonequal.grid(row=4, column=3)
-buttonclear.grid(row=5, column=3)
+buttonadd.grid(row=1, column=3)
+buttonequal.grid(row=4, column=2)
+buttonclear.grid(row=4, column=1)
+buttonmult.grid(row=2, column =3)
+buttonsub.grid(row=3, column=3)
+buttondiv.grid(row=4, column=3)
 
 
 
