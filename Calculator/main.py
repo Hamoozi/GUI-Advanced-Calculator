34000e35722de2c02c24f2fa4b1df15b9f9bbe7d
Hamoozi <moguristhebest@gmail.com>
2023-06-30 21:28:04 -0400

    fix: changed backround size to fit buttons

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,14 +1,12 @@
 from tkinter import *
 
-
-
 root = Tk()
 root.title("Calculator")
+root.geometry("500x500")
 
-e = Entry(root, width=35, borderwidth=5)
+e = Entry(root, width=35, borderwidth=5, bg='white')
 e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 counter = -1
-
 
 def button_click(number):
     global counter
@@ -64,24 +62,19 @@ def button_firstp(m):
 def button_secondp(m):
     global counter
     counter +=1
-    e.insert(counter, m)
-    
-    
+    e.insert(counter, m)  
 
-        
-    
 
-button1 = Button(root, text=1, padx= 60, pady=40, command=lambda: button_click(1))
-button2 = Button(root, text=2, padx= 60, pady=40, command=lambda: button_click(2))
-button3 = Button(root, text=3, padx= 60, pady=40, command=lambda: button_click(3))
-button4 = Button(root, text=4, padx= 60, pady=40, command=lambda: button_click(4))
-button5 = Button(root, text=5, padx= 60, pady=40, command=lambda: button_click(5))
-button6 = Button(root, text=6, padx= 60, pady=40, command=lambda: button_click(6))
-button7 = Button(root, text=7, padx= 60, pady=40, command=lambda: button_click(7))
-button8 = Button(root, text=8, padx= 60, pady=40, command=lambda: button_click(8))
-button9 = Button(root, text=9, padx= 60, pady=40, command=lambda: button_click(9))
-button0 = Button(root, text=0, padx= 60, pady=40, command=lambda: button_click(0))
-
+button1 = Button(root, text=1, padx=60, pady=40, command=lambda: button_click(1))
+button2 = Button(root, text=2, padx=60, pady=40, command=lambda: button_click(2))
+button3 = Button(root, text=3, padx=60, pady=40, command=lambda: button_click(3))
+button4 = Button(root, text=4, padx=60, pady=40, command=lambda: button_click(4))
+button5 = Button(root, text=5, padx=60, pady=40, command=lambda: button_click(5))
+button6 = Button(root, text=6, padx=60, pady=40, command=lambda: button_click(6))
+button7 = Button(root, text=7, padx=60, pady=40, command=lambda: button_click(7))
+button8 = Button(root, text=8, padx=60, pady=40, command=lambda: button_click(8))
+button9 = Button(root, text=9, padx=60, pady=40, command=lambda: button_click(9))
+button0 = Button(root, text=0, padx=60, pady=40, command=lambda: button_click(0))
 buttonadd = Button(root, text='+', padx=40, pady=40, command=lambda : button_add('+'))
 buttonequal = Button(root, text='=', padx=60, pady=40, command=lambda: button_equal())
 buttonclear = Button(root, text='clear', padx=60, pady=40, command=lambda: button_clear())
@@ -91,35 +84,29 @@ buttondiv = Button(root, text='÷', padx=40, pady=40, command=lambda: button_div
 buttonsq = Button(root, text='²', padx=40, pady=40, command=lambda: button_sq('**2'))
 buttonsqr = Button(root, text='√', padx=40, pady=40, command=lambda: button_sqroot('**(1/2)'))
 buttonp1 = Button(root, text='(', padx=40, pady=40, command=lambda: button_firstp('('))
-buttonp2 = Button(root, text=')', padx=40, pady=40, command=lambda: button_firstp(')'))
-
-
+buttonp2 = Button(root, text=')', padx=40, pady=40, command=lambda: button_secondp(')'))
 
 button1.grid(row=3, column=0)
 button2.grid(row=3, column=1)
 button3.grid(row=3, column=2)
-
 button4.grid(row=2, column=0)
 button5.grid(row=2, column=1)
 button6.grid(row=2, column=2)
-
 button7.grid(row=1, column=0)
 button8.grid(row=1, column=1)
 button9.grid(row=1, column=2)
-
 button0.grid(row=4, column=0)
-
 buttonadd.grid(row=1, column=3)
 buttonequal.grid(row=4, column=2)
 buttonclear.grid(row=4, column=1)
-buttonmult.grid(row=2, column =3)
+buttonmult.grid(row=2, column=3)
 buttonsub.grid(row=3, column=3)
 buttondiv.grid(row=4, column=3)
-buttonsq.grid(row=5, column=1)
-buttonsqr.grid(row=5, column=0)
-buttonp1.grid(row=5, column=2)
-buttonp2.grid(row=5, column=3)
+buttonsq.grid(row=1, column=4)
+buttonsqr.grid(row=2, column=4)
+buttonp1.grid(row=3, column=4)
+buttonp2.grid(row=4, column=4)
 
-
+root.mainloop()
 
 root.mainloop()
